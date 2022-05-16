060854fd6d1c005ea107a4f49df80aa488d034d6
otnielgusta <otnielsilvag4@gmail.com>
2022-05-16 13:30:31 -0300

    Aprendendo a usar o git diff

--- teste.py
+++ teste.py
@@ -2,6 +2,8 @@ def teste(emp_id:str, kind:str):
     depois(**{f"push__extra_data__emps_failed_{kind}": emp_id})
 
 def depois(**kwargs):
+    print("Adicionei essa linha")
     print(kwargs)
+
 
 teste(emp_id="employee.id", kind="exporter_generate_dict_task_bv_e09")
