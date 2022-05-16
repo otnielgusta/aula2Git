056b967bc9b923c0f21552ddc1198cfee4ad52bd
otnielgusta <otnielsilvag4@gmail.com>
2022-05-16 13:20:22 -0300

    criação do teste.py

---
+++ teste.py
@@ -0,0 +1,7 @@
+def teste(emp_id:str, kind:str):
+    depois(**{f"push__extra_data__emps_failed_{kind}": emp_id})
+
+def depois(**kwargs):
+    print(kwargs)
+
+teste(emp_id="employee.id", kind="exporter_generate_dict_task_bv_e09")
